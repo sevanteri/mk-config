79aeffbd4fa0cb85250f4816903c49a6cb503a82
Pete Sevander <pete.sevander@gmail.com>
2021-06-03 22:21:15 +0300

    zmk finnish keymap, keypositions and most importantly, COMBOS!

---
+++ zmk/config/finnish_keymap.h
@@ -0,0 +1,100 @@
+// finnish keymap
+
+// Row 1
+#define FI_SECT GRAVE // §
+#define FI_1    N1    // 1
+#define FI_2    N2    // 2
+#define FI_3    N3    // 3
+#define FI_4    N4    // 4
+#define FI_5    N5    // 5
+#define FI_6    N6    // 6
+#define FI_7    N7    // 7
+#define FI_8    N8    // 8
+#define FI_9    N9    // 9
+#define FI_0    N0    // 0
+#define FI_PLUS MINUS // +
+#define FI_ACUT EQUAL // ´ (dead)
+
+// Row 2
+#define FI_Q    Q    // Q
+#define FI_W    W    // W
+#define FI_E    E    // E
+#define FI_R    R    // R
+#define FI_T    T    // T
+#define FI_Y    Y    // Y
+#define FI_U    U    // U
+#define FI_I    I    // I
+#define FI_O    O    // O
+#define FI_P    P    // P
+#define FI_ARNG LEFT_BRACKET // Å
+#define FI_DIAE RIGHT_BRACKET // ¨ (dead)
+
+// Row 3
+#define FI_A    A    // A
+#define FI_S    S    // S
+#define FI_D    D    // D
+#define FI_F    F    // F
+#define FI_G    G    // G
+#define FI_H    H    // H
+#define FI_J    J    // J
+#define FI_K    K    // K
+#define FI_L    L    // L
+#define FI_ODIA SINGLE_QUOTE // Ö
+#define FI_ADIA SEMICOLON // Ä
+#define FI_QUOT BACKSLASH // '
+
+// Row 4
+#define FI_LABK NON_US_BACKSLASH // <
+#define FI_Z    Z    // Z
+#define FI_X    X    // X
+#define FI_C    C    // C
+#define FI_V    V    // V
+#define FI_B    B    // B
+#define FI_N    N    // N
+#define FI_M    M    // M
+#define FI_COMM COMMA // ,
+#define FI_DOT  DOT  // .
+#define FI_MINS SLASH // -
+
+
+// Shifted
+// Row 1
+#define FI_HALF LS(FI_SECT) // ½
+#define FI_EXLM LS(FI_1)    // !
+#define FI_DQUO LS(FI_2)    // "
+#define FI_HASH LS(FI_3)    // #
+#define FI_CURR LS(FI_4)    // ¤
+#define FI_PERC LS(FI_5)    // %
+#define FI_AMPR LS(FI_6)    // &
+#define FI_SLSH LS(FI_7)    // /
+#define FI_LPRN LS(FI_8)    // (
+#define FI_RPRN LS(FI_9)    // )
+#define FI_EQL  LS(FI_0)    // =
+#define FI_QUES LS(FI_PLUS) // ?
+#define FI_GRV  LS(FI_ACUT) // ` (dead)
+// Row 2
+#define FI_CIRC LS(FI_DIAE) // ^ (dead)
+// Row 3
+#define FI_ASTR LS(FI_QUOT) // *
+// Row 4
+#define FI_RABK LS(FI_LABK) // >
+#define FI_SCLN LS(FI_COMM) // ;
+#define FI_COLN LS(FI_DOT)  // :
+#define FI_UNDS LS(FI_MINS) // _
+
+// AltGr
+// Row 1
+#define FI_AT   RA(FI_2)    // @
+#define FI_PND  RA(FI_3)    // £
+#define FI_DLR  RA(FI_4)    // $
+#define FI_EURO RA(FI_5)    // €
+#define FI_LCBR RA(FI_7)    // {
+#define FI_LBRC RA(FI_8)    // [
+#define FI_RBRC RA(FI_9)    // ]
+#define FI_RCBR RA(FI_0)    // }
+#define FI_BSLS RA(FI_PLUS) // (backslash)
+// Row 2
+#define FI_TILD RA(FI_DIAE) // ~ (dead)
+// Row 4
+#define FI_PIPE RA(FI_LABK) // |
+#define FI_MICR RA(FI_M)    // µ
---
+++ zmk/config/keypos.h
@@ -0,0 +1,56 @@
+
+#define TOP 0
+#define MID 12
+#define BOT 24
+#define THM 40
+
+#define  P_TAB   TOP
+#define  P_Q     (TOP+1)
+#define  P_W     (TOP+2)
+#define  P_E     (TOP+3)
+#define  P_R     (TOP+4)
+#define  P_T     (TOP+5)
+#define  P_Y     (TOP+6)
+#define  P_U     (TOP+7)
+#define  P_I     (TOP+8)
+#define  P_O     (TOP+9)
+#define  P_P     (TOP+10)
+#define  P_QUOT  (TOP+11)
+
+#define  P_ESC  MID
+#define  P_A    (MID+1)
+#define  P_S    (MID+2)
+#define  P_D    (MID+3)
+#define  P_F    (MID+4)
+#define  P_G    (MID+5)
+#define  P_H    (MID+6)
+#define  P_J    (MID+7)
+#define  P_K    (MID+8)
+#define  P_L    (MID+9)
+#define  P_OE   (MID+10)
+#define  P_AE   (MID+11)
+
+#define  P_LSFT  BOT
+#define  P_Z     (BOT+1)
+#define  P_X     (BOT+2)
+#define  P_C     (BOT+3)
+#define  P_V     (BOT+4)
+#define  P_B     (BOT+5)
+#define  P_N     (BOT+10)
+#define  P_M     (BOT+11)
+#define  P_COMM  (BOT+12)
+#define  P_DOT   (BOT+13)
+#define  P_MINS  (BOT+14)
+#define  P_RSFT  (BOT+15)
+
+#define  P_TL_5  THM
+#define  P_TL_4  (THM+1)
+#define  P_TL_3  (THM+2)
+#define  P_TL_2  (THM+3)
+#define  P_TL_1  (THM+4)
+
+#define  P_TR_1  (THM+5)
+#define  P_TR_2  (THM+6)
+#define  P_TR_3  (THM+7)
+#define  P_TR_4  (THM+8)
+#define  P_TR_5  (THM+9)
